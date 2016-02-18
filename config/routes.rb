a9f466e4798b214014a9b67d675c30fc4201aab6
Alice Suckling <alicesuckling@live.co.uk>
2016-02-18 14:55:11 +0000

    sign up, in and out plus validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # 
-  # get "users/show"
-  # get "welcome/index"
+
+  # resources :users, only: [:update, :show, :index]
+  get "welcome/index"
+  get "users/index"
+  get "users/show"
+
 
 end
